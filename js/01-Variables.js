bae77b13006215043bbb947fe7d7f6d270cd6cf2
Luis Robles <luisgeremiasrobles@gmail.com>
2025-06-03 21:30:59 -0300

    Se declararon algunas variables

---
+++ js/01-Variables.js
@@ -0,0 +1,15 @@
+//Este es un comentario de una sola linea
+
+/*Este es un cometario
+de
+multiples lineas
+*/
+
+//Crear variables (let - const - var)
+let numero1;
+
+let year = 2025;
+
+//Mostrar el valor de las variables
+console.log("Hola Mundo!!");
+console.log(numero1,year);
