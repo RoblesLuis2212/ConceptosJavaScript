fe289766ca858590c069225dd21d5683398446f6
Luis Robles <luisgeremiasrobles@gmail.com>
2025-06-05 21:34:20 -0300

    Se vio uso de lenght, substring y charAt

---
+++ js/Ejercicios/02-Tp1-Ejercicio9.js
@@ -0,0 +1,11 @@
+//Escribe un programa que pida una frase y escriba las vocales que aparecen
+let Frase = prompt("Ingrese una frase: ");
+
+console.log(Frase);
+//palabra de prueba "hola"
+console.log(Frase.length);
+
+console.log(Frase.substring(0,3));
+
+console.log(Frase.charAt(0));
+console.log(Frase.charAt(5));
