3118219dd3812df172fc142a4182c1a3fd49d45b
Luis Robles <luisgeremiasrobles@gmail.com>
2025-06-10 20:40:35 -0300

    Se comenzo a desarrollar el programa del cajero automatico

---
+++ js/03-Switch.js
@@ -0,0 +1,49 @@
+//Cree un programa al estilo de un cajero automatico con las siguientes opciones:
+//1- consultar el saldo
+//2- ingresar dinero
+//3- extraer dinero
+
+
+/* switch(opcion){
+    case 1:
+        Todas las lineas de codigo que quiero hacer si la opcion de usuario es 1;
+        break
+        case 2:
+            Todas las lineas de codigo que quiero hacer si la opcion de usuario es 1;
+            break
+            case N:
+                Todas las lineas de codigo que quiero hacer si la opcion de usuario es N;
+                break
+                default:
+                    Todas las lineas a ejecutar si las condiciones anteriores no se cumplen
+                    }
+                    */
+const opcion = prompt("Seleccione una opcion: 1- consultar saldo 2- ingresar dinero 3- extraer dinero");
+let Saldo = 10000;
+
+switch(opcion){
+    case "1":
+        document.writeln("Su saldo es de $"+Saldo+"");
+        break;
+    case "2":
+        const deposito = parseFloat(prompt("Ingrese la cantidad de dinero a depositar"));
+        if (deposito > 1000 && deposito < 5000000){
+            Saldo = Saldo + deposito;
+            alert(`Depositaste $${deposito}, tu saldo actual es $${Saldo}`);
+        }
+        else{
+            alert("Ingresaste un monto invalido");
+        }
+        break;
+    case "3":
+        const monto_extraccion = parseFloat(prompt("Ingrese el monto a extraer: "));
+        if (monto_extraccion > Saldo){
+            alert("Fondos insuficientes");
+        }
+        else{
+            Saldo = Saldo - monto_extraccion;
+        }
+        break;
+    default:
+        alert("Opcion incorrecta, ingrese una opcion valida");
+}
