b4389a4222d2939f383b5e881d2b9f54340bd216
Luis Robles <luisgeremiasrobles@gmail.com>
2025-06-12 20:36:53 -0300

    Se mejoro el switch con un bucle do-while

--- js/03-Switch.js
+++ js/03-Switch.js
@@ -17,36 +17,42 @@
                 default:
                     Todas las lineas a ejecutar si las condiciones anteriores no se cumplen
                     }
+
                     */
-const opcion = prompt("Seleccione una opcion: 1- consultar saldo 2- ingresar dinero 3- extraer dinero");
+
+
 let Saldo = 10000; //Se define un saldo por defecto
 
-//Agregamos una estructura switch para evaluar las opciones
-switch(opcion){
-    case "saldo":
-    case "1":
-        document.writeln("Su saldo es de $"+Saldo+"");
-        break;
-    case "2":
-        const deposito = parseFloat(prompt("Ingrese la cantidad de dinero a depositar"));
-        if (deposito > 1000 && deposito < 5000000){
-            Saldo = Saldo + deposito;
-            alert(`Depositaste $${deposito}, tu saldo actual es $${Saldo}`);
-        }
-        else{
-            alert("Ingresaste un monto invalido");
-        }
-        break;
-    case "3":
-        const monto_extraccion = parseFloat(prompt("Ingrese el monto a extraer: "));
-        if (monto_extraccion <= Saldo){
-            Saldo = Saldo - monto_extraccion;
-            alert(`Extraccion exitosa, tu saldo disponible es ${Saldo}`);
-        }
-        else{
-            alert("Fondos insuficientes");
-        }
-        break;
-    default:
-        alert("Opcion incorrecta, ingrese una opcion valida");
+do{
+    const opcion = prompt("Seleccione una opcion: 1- consultar saldo 2- ingresar dinero 3- extraer dinero");
+    //Agregamos una estructura switch para evaluar las opciones
+    switch(opcion){
+        case "saldo":
+        case "1":
+            document.writeln("Su saldo es de $"+Saldo+"");
+            break;
+        case "2":
+            const deposito = parseFloat(prompt("Ingrese la cantidad de dinero a depositar"));
+            if (deposito > 1000 && deposito < 5000000){
+                Saldo = Saldo + deposito;
+                alert(`Depositaste $${deposito}, tu saldo actual es $${Saldo}`);
+            }
+            else{
+                alert("Ingresaste un monto invalido");
+            }
+            break;
+        case "3":
+            const monto_extraccion = parseFloat(prompt("Ingrese el monto a extraer: "));
+            if (monto_extraccion <= Saldo){
+                Saldo = Saldo - monto_extraccion;
+                alert(`Extraccion exitosa, tu saldo disponible es ${Saldo}`);
+            }
+            else{
+                alert("Fondos insuficientes");
+            }
+            break;
+        default:
+            alert("Opcion incorrecta, ingrese una opcion valida");
+    }
 }
+while(confirm("¿Quieres realizar otra operacion?"));
