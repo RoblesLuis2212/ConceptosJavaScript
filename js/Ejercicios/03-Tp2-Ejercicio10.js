a72b081672ea6c4b54afdb42678683a8fd1c4cc0
Luis Robles <luisgeremiasrobles@gmail.com>
2025-06-17 19:17:19 -0300

    Nose que onda la tabla

---
+++ js/Ejercicios/03-Tp2-Ejercicio10.js
@@ -0,0 +1,18 @@
+//Realiza un script que pida número de filas y columnas y escriba una tabla.
+//Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente.7
+//Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
+
+//Pedir al usuario las filas y columnas
+
+//dibujar la tabla
+document.writeln(`<table>
+      <tbody>
+      `);
+document.writeln(`
+    <tr>
+    <td>33</td>
+    </tr>
+    `);
+document.writeln(`
+      </tbody>
+    </table>`);
