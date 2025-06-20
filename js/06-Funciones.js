cead8ca053915b297884491c3f6ef3b2d4bc418b
Luis Robles <luisgeremiasrobles@gmail.com>
2025-06-19 21:14:20 -0300

    Agregamos un ejemplo de funcion sin parametro y con parametro

---
+++ js/06-Funciones.js
@@ -0,0 +1,28 @@
+//Una funcion es un bloque de codigo que cumple una funcion espefica dentro del programa
+//Permite reutilizar codigo
+
+//Funciones tradicionales
+function Saludar() {
+  //Todas las lineas de codigo que sabe hacer esta funcion
+  document.writeln(`<p>Hola Mundo🌍</p>`);
+}
+//Funcion con parametros
+function Saludar_Persona(nombre, apellido) {
+  document.writeln(`<p>Hola ${nombre} ${apellido}</p>`);
+}
+
+//Expresion de funcion
+
+//Arrow function, funciones flecha
+
+//Invocar a la funcion
+Saludar();
+Saludar();
+Saludar();
+Saludar();
+Saludar();
+
+//Solicitamos datos al usuario y pasamos los parametros que solicita la funcion
+const Nombre = prompt("Ingrese su nombre: ");
+const Apellido = prompt("Ingrese su apellido: ");
+Saludar_Persona(Nombre, Apellido);
