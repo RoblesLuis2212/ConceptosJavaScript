0652b4e5862f8761e638c816e8e0ab42225e7e89
Luis Robles <luisgeremiasrobles@gmail.com>
2025-07-01 21:42:18 -0300

    Agregamos un ejemplo de encapsulamiento y herencia

--- js/08-Clases.js
+++ js/08-Clases.js
@@ -7,6 +7,7 @@
 //Creacion de la clase persona
 class Persona {
   //Metodo constructor se ejecuta automaticamente al instanciar la clase
+  #email;
   constructor(
     nombre,
     apellido,
@@ -22,22 +23,28 @@ class Persona {
     this.fechaNacimiento = fechaNacimiento;
     this.provincia = provincia;
     this.dni = dni;
-    this.email = email;
+    this.#email = email;
     this.telefono = telefono;
     this.password = password;
     this.estado = true; //Valor por defecto
   }
+  //Propiedades getters y setters
+  get getEmail() {
+    return this.#email;
+  }
+  set setEmail(nuevoEmail) {
+    this.#email = nuevoEmail;
+  }
   //Metodos
   MostrarDatos() {
     //Todo lo que quiero que haga el metodo
-    document.writeln(`<h2>Datos Personales</h2`);
+    document.writeln(`<h2>Datos Personales</h2>`);
     document.writeln(`<ul>`);
     document.writeln(
       `<li>Nombre y Apellido: ${this.nombre} ${this.apellido}</li>`
     );
-    document.writeln(`<li>Email: ${this.email}</li>`);
+    document.writeln(`<li>Email: ${this.getEmail}</li>`);
     document.writeln(`<li>DNI: ${this.dni}</li>`);
-    document.writeln(`<li>Email: ${this.email}</li>`);
     document.writeln(`</ul>`);
   }
   Calcular_Edad() {}
@@ -49,6 +56,38 @@ class Persona {
   }
 }
 
+class Alumno extends Persona {
+  #notas;
+  constructor(
+    nombre,
+    apellido,
+    fechaNacimiento,
+    provincia,
+    dni,
+    email,
+    telefono,
+    password,
+    comision
+  ) {
+    //Invoca al constructor de persona
+    super(
+      nombre,
+      apellido,
+      fechaNacimiento,
+      provincia,
+      dni,
+      email,
+      telefono,
+      password
+    );
+    this.comision = comision;
+    this.insignias = [];
+    this.rollingCoins = 0;
+    this.rollingCoins = 0;
+    this.asistencia = 0;
+    this.#notas = [];
+  }
+}
 //Logica del programa
 //Instanciamos la clase Persona y creamos un objeto
 const maxi = new Persona(
@@ -59,8 +98,7 @@ const maxi = new Persona(
   "40254874",
   "maxigomeztolrra@gmail.com",
   "-",
-  "3863457852",
-  "%ABC12345"
+  "3863457852"
 );
 const agus = new Persona(
   "Agustina",
@@ -70,11 +108,18 @@ const agus = new Persona(
   "42798248",
   "agusbulacion2020@gmail.com",
   "-",
-  "3884616131",
-  "%ABC12345"
+  "3884616131"
 );
 
 console.log(maxi);
 //Invocacion al metodo de la clase
 maxi.MostrarDatos();
 agus.MostrarDatos();
+
+maxi.MostrarDatos();
+
+//Mostrar propiedad encapsulada con el metodo get
+document.writeln(`<p>Consultar email: ${maxi.getEmail}</p>`);
+
+maxi.setEmail = "maxi@gmail.com";
+maxi.MostrarDatos();
